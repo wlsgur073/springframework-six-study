d7555e2899975f43de677fdd65ae24a10e8a0621
wlsgur073 <wlsgur073@gmail.com>
2024-12-01 12:40:48 +0900

    `Provider`라는 명칭으로 클래스 분리. 결국 클래스와 메소드가 rename 될 때마다 코드를 수정해야 되는 번거로움 발생.

--- src/main/java/springsix/spring6/Client.java
+++ src/main/java/springsix/spring6/Client.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        PaymentService service = new WebApiExRatePaymentService();
+        PaymentService service = new PaymentService();
         Payment payment = service.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println("payment = " + payment);
     }
