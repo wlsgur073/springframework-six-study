9839154fbf9fa716dcb360ea6fece61c8bcfa7e5
wlsgur073 <wlsgur073@gmail.com>
2024-12-01 13:11:33 +0900

    인터페이스를 도입하고 관계설정 책임을 분리한다. 관계설정이란 인터페이스의 구현체의 생성자를 어디서 호출할 지, 그 책임을 설정하는 것을 의미한다.

--- src/main/java/springsix/spring6/PaymentService.java
+++ src/main/java/springsix/spring6/PaymentService.java
@@ -5,14 +5,14 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class PaymentService {
-    private final WebApiExRateProvider exRateProvider;
+    private final ExRateProvider exRateProvider;
 
-    public PaymentService() {
-        this.exRateProvider = new WebApiExRateProvider();
+    public PaymentService(ExRateProvider exRateProvider) {
+        this.exRateProvider = exRateProvider;
     }
 
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-        BigDecimal exRate = exRateProvider.getWebExRate(currency);
+        BigDecimal exRate = exRateProvider.getExRate(currency);
         BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
         LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
 
